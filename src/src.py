f1fc5b7987ad77ffcfe04fcc40d77f772a18de1c
camilovelezr <camilov61@gmail.com>
2021-11-14 18:26:28 -0500

    working image in canvas

--- src/src.py
+++ src/src.py
@@ -1,3 +1,53 @@
-#! usr/bin/python
+import google_auth_httplib2
+from gphotospy import album, authorize
 
-"""Hello"""
+import io
+from tkinter import *
+from urllib.request import urlopen
+
+from gphotospy.authorize import get_credentials
+from PIL import ImageTk, Image
+
+CLIENT_SECRET_FILE = "credentials.json"
+
+service = authorize.init(CLIENT_SECRET_FILE)
+
+from gphotospy.album import *
+
+album_manager = Album(service)
+
+album_iterator = album_manager.list()
+
+def find_album():
+    for n in album_iterator:
+        if (n.get("title")=="La LW"):
+            return n.get("id")
+
+_id = find_album()
+
+from gphotospy.media import *
+
+media_manager = Media(service)
+
+album_media_list = list(media_manager.search_album(_id))
+
+len(album_media_list)
+
+url = album_media_list[10].get("baseUrl")
+
+
+root = Tk()
+
+canvas = Canvas(root, widt=800, height=600, bg='white')
+
+canvas.pack(side='top', fill='both', expand='yes')
+
+im_url = f"{url}=w800-h600"
+
+img_bytes = urlopen(im_url).read()
+
+img = Image.open(io.BytesIO(img_bytes))
+
+photo = ImageTk.PhotoImage(img)
+
+canvas.create_image(10, 10, image=photo, anchor='nw')
